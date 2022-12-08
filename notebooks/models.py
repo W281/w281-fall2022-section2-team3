1935d18efd9cda03fda2725e6b86857f29ac5e64
Hoon Kim <yhkim@blizzard.com>
2022-12-08 09:49:10 -0800

    optimize function updated in models.py

--- notebooks/models.py
+++ notebooks/models.py
@@ -1,4 +1,7 @@
 import torchvision.models as models
+
+from hyperopt import STATUS_OK,rand, tpe, Trials, fmin, hp
+from hyperopt.early_stop import no_progress_loss
 from torch import nn
 
 class ResNet152(nn.Module):
@@ -26,3 +29,50 @@ class LogisticRegression(nn.Module):
      def forward(self, x):
          outputs = torch.sigmoid(self.model(x))
          return outputs
+    
+# Optimizer definition
+def optimize(opt_dict, best_dict, X_train, y_train, max_evals = 50, scoring_fn = 'neg_log_loss' random_state = config.SEED):
+    """
+        Runs hyperopt for all the models in opt_dict. Adds the best hyperparameter set for each model.
+        Returns dictionary of best hyperparameter set.
+    """
+    # Define TPE algorithm for all optimizers
+    tpe_algo = tpe.suggest
+
+    # Iterate over opt_dict
+    for k,v in opt_dict.items():
+        ## Attributes
+        model_name = k
+        model = v['model']
+        params = v['params']
+
+        ## objective function definition
+        def f(params):
+            loss = None
+            try:
+                m = model(random_state = random_state, **params)
+                loss = -cross_val_score(m, X_train, y_train, scoring = scoring_fn).mean()
+            except: AttributeError
+
+            return {'loss': loss, 'status': STATUS_OK}
+
+        ## Define trial space
+        trials = Trials()
+
+        print(f"Optimizing {k} model...")
+
+        ## optimize
+        best = fmin(
+            fn = f,
+            space = params,
+            algo = tpe_algo,
+            max_evals = max_evals,
+            trials = trials,
+            early_stop_fn=no_progress_loss(1e-10)
+        )
+
+        best_dict[model_name] = best
+        print(f"Best hyperparameter set for {model_name} is {best}")
+        print("\n")
+
+    return best_dict
